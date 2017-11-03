a14d9afdb7c4664a2e33d32cccfdf0b69e9e4341
Julian Niebieskikwiat <juliannieb@gmail.com>
2017-11-03 16:54:46 -0600

    Add properties and methods for Watch class

--- app/src/main/java/com/example/juliannieb/dastodolistproject/Classes/Watch.kt
+++ app/src/main/java/com/example/juliannieb/dastodolistproject/Classes/Watch.kt
@@ -3,3 +3,64 @@ package com.example.juliannieb.dastodolistproject.Classes
 /**
  * Created by juliannieb on 11/3/17.
  */
+
+class Watch {
+
+    var hour : Int
+        get() = this.hour
+        set(hour) {
+            this.hour = hour
+        }
+
+    var minute : Int
+        get() = this.minute
+        set(minute) {
+            this.minute = minute
+        }
+
+    var second : Int
+        get() = this.second
+        set(second) {
+            this.second = second
+        }
+
+    constructor() {
+        this.hour = 0
+        this.minute = 0
+        this.second = 0
+    }
+
+    constructor(hour : Int, minute : Int, second : Int) {
+        this.hour = hour
+        this.minute = minute
+        this.second = second
+    }
+
+    fun tickForwards() {
+        this.second++
+        if (this.second == 60) {
+            this.second = 0
+            this.minute++
+            if (this.minute == 60) {
+                this.minute = 0
+                this.hour++
+            }
+        }
+    }
+
+    fun tickBackwards() {
+        if (this.hour == 0 && this.minute == 0 && this.second == 0){
+            return
+        }
+        this.second--
+        if (this.second == 0) {
+            this.second = 59
+            this.minute--
+            if (this.minute == 0) {
+                this.minute = 59
+                this.hour--
+            }
+        }
+    }
+    
+}
