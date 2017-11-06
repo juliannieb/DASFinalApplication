744e2527785d86d19ff4ec964e70672bdac33f91
Julian Niebieskikwiat <juliannieb@gmail.com>
2017-11-06 17:38:06 -0600

    Remove unnecessary imports

--- app/src/main/java/com/example/juliannieb/dastodolistproject/TimerActivity.kt
+++ app/src/main/java/com/example/juliannieb/dastodolistproject/TimerActivity.kt
@@ -7,9 +7,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import com.example.juliannieb.dastodolistproject.Classes.Timer
-import com.example.juliannieb.dastodolistproject.R
 
 class TimerActivity : AppCompatActivity() {
 
